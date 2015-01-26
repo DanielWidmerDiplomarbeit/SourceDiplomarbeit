a57a3ea82e88e8d70f147a832b8091da5ca6c4e2
DanielWidmer <danielwidmer@gmx.ch>
2015-01-26 19:22:11 +0100

    Android weggelöscht, IPad Horizontal

--- TodoMvvm.iOS/AppDelegate.cs
+++ TodoMvvm.iOS/AppDelegate.cs
@@ -9,21 +9,10 @@ using System.IO;
 
 namespace TodoMvvm
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the
-	// User Interface of the application, as well as listening (and optionally responding) to
-	// application events from iOS.
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
-		// class-level declarations
 		UIWindow window;
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			Forms.Init ();
@@ -40,7 +29,6 @@ namespace TodoMvvm
 			if (!File.Exists (path)) {
 				File.Copy (sqliteFilename, path);
 			}
-
 
 			var plat = new SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS();
 			var conn = new SQLite.Net.SQLiteConnection(plat, path);
