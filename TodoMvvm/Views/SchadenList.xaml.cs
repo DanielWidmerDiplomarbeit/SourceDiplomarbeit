894278558497c8ad5b9b6a87427dd62eae30dfcb
DanielWidmer <danielwidmer@gmx.ch>
2015-06-13 13:38:09 +0200

    Erster Commit von Windows

---
+++ TodoMvvm/Views/SchadenList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Platform;
+using ZeusMobile.ViewModels;
+
+namespace ZeusMobile.Views
+{
+    public partial class SchadenList : ContentPage
+    {
+        public SchadenList()
+        {
+            InitializeComponent();
+        }
+    }
+}
